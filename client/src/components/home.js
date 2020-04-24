239db7e8f2d0f3a4096cd1cda3cb66b6a75fe938
Hayden-Neilson <haydenneilson5@gmail.com>
2020-04-24 11:14:21 -0600

    home page stles

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div className="App">
-            <h1>we got this</h1>
-                <div className="set-background">
-                <img src="https://wallpaperaccess.com/full/1858093.jpg"/>
-                <h2>this is going to work</h2>
-                </div>
-            <div>
-                
-            </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to Crud Donuts</h1>
+        <div className="set-background"></div>
+        <div className="front-image">
+          <img
+            src="https://www.voodoodoughnut.com/wp-content/uploads/2017/11/grape-ape-yeast-doughnut-side-400x400.jpg"
+            alt="Donut"
+          />
+        </div>
+        <div>
+          <h2>This is your last stop for donuts youll ever need</h2>
+        </div>
+      </div>
+    );
+  }
 }
